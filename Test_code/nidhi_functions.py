7123d2da864421c50a33ee654287bb7e721c2adc
kulbir-ahluwalia <kulbir@terpmail.umd.edu>
2020-03-22 03:46:17 -0400

    solved errors in intersection_check.py

--- Test_code/nidhi_functions.py
+++ Test_code/nidhi_functions.py
@@ -3,8 +3,9 @@ from sympy import solve, Poly, Eq, Function, exp
 from sympy.abc import x, y, z, a, b
 import math
 
+
 def find_line_slope_and_intercept(test_point_coord, line_point_1, line_point_2):
-    if (line_point_2[0] - line_point_1[0])==0:
+    if (line_point_2[0] - line_point_1[0]) == 0:
         x_intercept = line_point_1[0]
         return math.inf, x_intercept
     else:
@@ -12,7 +13,7 @@ def find_line_slope_and_intercept(test_point_coord, line_point_1, line_point_2):
         y_intercept = line_point_1[1] - (slope * line_point_1[0])
         return slope, y_intercept
 
-    # print(slope,intercept)
+    # #print(slope,intercept)
 
 
 def circular_intersection_check(clearance, radius_rigid_robot, parent_coord, child_coord):
@@ -20,25 +21,30 @@ def circular_intersection_check(clearance, radius_rigid_robot, parent_coord, chi
 
     line_m_c = find_line_slope_and_intercept(None, child_coord, parent_coord)
     line_equation = y - (line_m_c[0] * x) - (line_m_c[1])
-    #print(line_equation)
+    ##print(line_equation)
 
     circle_center = (225, 150)
     equation_1 = (x - 225) ** 2 + (y - 150) ** 2 - (25 + augment_distance) ** 2
     equation_2 = line_equation
     # the following equation will output solutions in the form of a list
     solution = solve([equation_1, equation_2], (x, y))
-    #print(solution)
+    ##print(solution)
 
+    not_intersecting = True
     for root in solution:
         x_max = max(parent_coord[0], child_coord[0])
         x_min = min(parent_coord[0], child_coord[0])
         y_max = max(parent_coord[1], child_coord[1])
         y_min = min(parent_coord[1], child_coord[1])
 
+
         if (x_min <= root[0] <= x_max) and (y_min <= root[1] <= y_max):
-            return False  # intersection present, not valid vector
+            not_intersecting = False
+            break  # intersection present, not valid vector
         else:
-            return True
+            not_intersecting = True
+
+    return not_intersecting
 
 
 def ellipse_intersection_check(clearance, radius_rigid_robot, parent_coord, child_coord):
@@ -50,27 +56,31 @@ def ellipse_intersection_check(clearance, radius_rigid_robot, parent_coord, chil
     else:
         line_equation = y - (line_m_c[0] * x) - (line_m_c[1])
 
-    print(line_equation)
+    # #print(line_equation)
     a = 40 + augment_distance
     b = 20 + augment_distance
     center = (150, 100)
-    equation_1 = ((x - center[0]) ** 2 / (b ** 2)) + ((y - center[1]) ** 2 / (a ** 2)) - 1
+    equation_1 = ((x - center[0]) ** 2 / (a ** 2)) + ((y - center[1]) ** 2 / (b ** 2)) - 1
     equation_2 = line_equation
     # the following equation will output solutions in the form of a list
     solution2 = solve([equation_1, equation_2], (x, y))
     # print(solution2)
 
+    not_intersecting = True
     for root in solution2:
         x_max = max(parent_coord[0], child_coord[0])
         x_min = min(parent_coord[0], child_coord[0])
         y_max = max(parent_coord[1], child_coord[1])
         y_min = min(parent_coord[1], child_coord[1])
 
-        if (x_min <= root[0] <= x_max) and (y_min <= root[1] <= y_max):
-            return False  # intersection present, not valid vector
-        else:
-            return True
 
+        if (x_min <= root[0] <= x_max) and (y_min <= root[1] <= y_max):
+            not_intersecting = False
+            break  # intersection present, not valid vector
+        else:
+            not_intersecting = True
+
+    return not_intersecting
 
 
 def rectangle_intersection(clearance, radius_rigid_robot, parent_coord, child_coord):
@@ -102,26 +112,33 @@ def rectangle_intersection(clearance, radius_rigid_robot, parent_coord, child_co
     line4 = y - (edge4_m_c[0] * x) - (edge4_m_c[1] - (augment_distance * 2))
 
     solution3 = solve([line1, line_equation], (x, y))
-    print("Intersection points with first line: ", solution3)
+    # print("Intersection points with first line: ", solution3)
     solution4 = solve([line2, line_equation], (x, y))
-    print("Intersection points with second line: ", solution4)
+    # print("Intersection points with second line: ", solution4)
     solution5 = solve([line3, line_equation], (x, y))
-    print("Intersection points with third line: ", solution5)
+    # print("Intersection points with third line: ", solution5)
     solution6 = solve([line4, line_equation], (x, y))
-    print("Intersection points with Fourth line: ", solution6)
+    # print("Intersection points with Fourth line: ", solution6)
 
     solution = [solution3, solution4, solution5, solution6]
 
+    not_intersecting = True
     for root in solution:
+        # print(root)
         x_max = max(parent_coord[0], child_coord[0])
         x_min = min(parent_coord[0], child_coord[0])
         y_max = max(parent_coord[1], child_coord[1])
         y_min = min(parent_coord[1], child_coord[1])
 
+
         if (x_min <= root[x] <= x_max) and (y_min <= root[y] <= y_max):
-            return False  # intersection present, not valid vector
+            not_intersecting = False
+            # break  # intersection present, not valid vector
         else:
-            return True
+            not_intersecting = True
+
+    return not_intersecting
+
 
 def rhombus_intersection(clearance, radius_rigid_robot, parent_coord, child_coord):
     augment_distance = radius_rigid_robot + clearance
@@ -132,15 +149,15 @@ def rhombus_intersection(clearance, radius_rigid_robot, parent_coord, child_coor
     rhombus_point_4 = [225, 10]
 
     line_m_c = find_line_slope_and_intercept(None, child_coord, parent_coord)
-    if line_m_c[0]==math.inf:
-        line_equation = x - line_m_c[1]    # x- x_intercept
+    if line_m_c[0] == math.inf:
+        line_equation = x - line_m_c[1]  # x- x_intercept
     else:
         line_equation = y - (line_m_c[0] * x) - (line_m_c[1])
 
     # We set the flags by testing for image point inside the rectangle
     # Because the sign for the half plane is unique for every line, we test it by using image point that is confirmed to be inside the rectangle
     edge1_m_c = find_line_slope_and_intercept(None, rhombus_point_1, rhombus_point_2)
-    if edge1_m_c[0]==math.inf:    #not needed btw, since rhombus sides are always at an angle, slope is not infinity
+    if edge1_m_c[0] == math.inf:  # not needed btw, since rhombus sides are always at an angle, slope is not infinity
         line1 = x - edge1_m_c[1]
     else:
         line1 = y - (edge1_m_c[0] * x) - (edge1_m_c[1] + (augment_distance / 0.8575))
@@ -155,26 +172,31 @@ def rhombus_intersection(clearance, radius_rigid_robot, parent_coord, child_coor
     line4 = y - (edge4_m_c[0] * x) - (edge4_m_c[1] - (augment_distance / 0.8575))
 
     solution7 = solve([line1, line_equation], (x, y))
-    print("Intersection points with first line: ", solution7)
+    # print("Intersection points with first line: ", solution7)
     solution8 = solve([line2, line_equation], (x, y))
-    print("Intersection points with second line: ", solution8)
+    # print("Intersection points with second line: ", solution8)
     solution9 = solve([line3, line_equation], (x, y))
-    print("Intersection points with third line: ", solution9)
+    # print("Intersection points with third line: ", solution9)
     solution10 = solve([line4, line_equation], (x, y))
-    print("Intersection points with Fourth line: ", solution10)
+    # print("Intersection points with Fourth line: ", solution10)
 
     solution = [solution7, solution8, solution9, solution10]
 
+    not_intersecting = True
     for root in solution:
         x_max = max(parent_coord[0], child_coord[0])
         x_min = min(parent_coord[0], child_coord[0])
         y_max = max(parent_coord[1], child_coord[1])
         y_min = min(parent_coord[1], child_coord[1])
 
+
         if (x_min <= root[x] <= x_max) and (y_min <= root[y] <= y_max):
-            return False  # intersection present, not valid vector
+            not_intersecting = False
+            # break  # intersection present, not valid vector
         else:
-            return True
+            not_intersecting = True
+
+    return not_intersecting
 
 
 def polygon_right_intersection(clearance, radius_rigid_robot, parent_coord, child_coord):
@@ -208,28 +230,33 @@ def polygon_right_intersection(clearance, radius_rigid_robot, parent_coord, chil
     line5 = y - (edge5_m_c[0] * x) - (edge5_m_c[1] - (augment_distance / 0.640184))
 
     solution11 = solve([line1, line_equation], (x, y))
-    print("Intersection points with first line: ", solution11)
+    # print("Intersection points with first line: ", solution11)
     solution12 = solve([line2, line_equation], (x, y))
-    print("Intersection points with second line: ", solution12)
+    # print("Intersection points with second line: ", solution12)
     solution13 = solve([line3, line_equation], (x, y))
-    print("Intersection points with third line: ", solution13)
+    # print("Intersection points with third line: ", solution13)
     solution14 = solve([line4, line_equation], (x, y))
-    print("Intersection points with Fourth line: ", solution14)
+    # print("Intersection points with Fourth line: ", solution14)
     solution15 = solve([line5, line_equation], (x, y))
-    print("Intersection points with Fourth line: ", solution15)
+    # print("Intersection points with Fourth line: ", solution15)
 
     solution = [solution11, solution12, solution13, solution14, solution15]
 
+    not_intersecting = True
     for root in solution:
         x_max = max(parent_coord[0], child_coord[0])
         x_min = min(parent_coord[0], child_coord[0])
         y_max = max(parent_coord[1], child_coord[1])
         y_min = min(parent_coord[1], child_coord[1])
 
+
         if (x_min <= root[x] <= x_max) and (y_min <= root[y] <= y_max):
-            return False  # intersection present, not valid vector
+            not_intersecting = False
+            break  # intersection present, not valid vector
         else:
-            return True
+            not_intersecting = True
+
+    return not_intersecting
 
 
 def polygon_left_intersection(clearance, radius_rigid_robot, parent_coord, child_coord):
@@ -245,53 +272,55 @@ def polygon_left_intersection(clearance, radius_rigid_robot, parent_coord, child
 
     # We set the flags by testing for image point inside the rectangle
     # Because the sign for the half plane is unique for every line, we test it by using image point that is confirmed to be inside the nonconvex_obstacle
-    edge1_m_c = find_line_slope_and_intercept(test_point_coord, nonconvex_point_1, nonconvex_point_2)
+    edge1_m_c = find_line_slope_and_intercept(None, nonconvex_point_1, nonconvex_point_2)
     line1 = y - (edge1_m_c[0] * x) - (edge1_m_c[1] - (augment_distance / 0.27472))
 
-    edge2_m_c = find_line_slope_and_intercept(test_point_coord, nonconvex_point_2, nonconvex_point_3)
+    edge2_m_c = find_line_slope_and_intercept(None, nonconvex_point_2, nonconvex_point_3)
     line2 = y - (edge2_m_c[0] * x) - (edge2_m_c[1] + (augment_distance / 1))
 
     # edge 3 is not augmented with clearance+robot_radius since its inside the nonconvex polygon
-    edge3_m_c = find_line_slope_and_intercept(test_point_coord, nonconvex_point_3, nonconvex_point_4)
+    edge3_m_c = find_line_slope_and_intercept(None, nonconvex_point_3, nonconvex_point_4)
     line3 = y - (edge3_m_c[0] * x) - (edge3_m_c[1] + (augment_distance / 0.0767))
 
-    edge4_m_c = find_line_slope_and_intercept(test_point_coord, nonconvex_point_4, nonconvex_point_1)
+    edge4_m_c = find_line_slope_and_intercept(None, nonconvex_point_4, nonconvex_point_1)
     line4 = y - (edge4_m_c[0] * x) - (edge4_m_c[1] - (augment_distance / 0.7071))
 
     solution16 = solve([line1, line_equation], (x, y))
-    print("Intersection points with first line: ", solution16)
+    # print("Intersection points with first line: ", solution16)
     solution17 = solve([line2, line_equation], (x, y))
-    print("Intersection points with second line: ", solution17)
+    # print("Intersection points with second line: ", solution17)
     solution18 = solve([line3, line_equation], (x, y))
-    print("Intersection points with third line: ", solution18)
+    # print("Intersection points with third line: ", solution18)
     solution19 = solve([line4, line_equation], (x, y))
-    print("Intersection points with Fourth line: ", solution19)
-    solution20 = solve([line5, line_equation], (x, y))
-    print("Intersection points with Fourth line: ", solution20)
+    # print("Intersection points with Fourth line: ", solution19)
 
-    solution = [solution16, solution17, solution18, solution19, solution20]
+    solution = [solution16, solution17, solution18, solution19]
 
+    not_intersecting = True
     for root in solution:
         x_max = max(parent_coord[0], child_coord[0])
         x_min = min(parent_coord[0], child_coord[0])
         y_max = max(parent_coord[1], child_coord[1])
         y_min = min(parent_coord[1], child_coord[1])
 
+
         if (x_min <= root[x] <= x_max) and (y_min <= root[y] <= y_max):
-            return False  # intersection present, not valid vector
+            not_intersecting = False
+            break  # intersection present, not valid vector
         else:
-            return True
+            not_intersecting = True
 
-print(circular_intersection_check(1,1,[197,150],[253,150]))
-
-print(ellipse_intersection_check(1,1,[200,150],[250,150]))
-
-print(rectangle_intersection(1,1,[200,150],[250,150]))
+    return not_intersecting
 
 
-print(rhombus_intersection(1,1,[190,24],[255,26]))   #False case, horizontal vector cuts the rhombus
-print(rhombus_intersection(1,1,[225,5],[225,50]))   #False case, vertical vector cuts the rhombus
+print(circular_intersection_check(1, 1, [197, 150], [253, 150]))  # should give False
 
-print(polygon_right_intersection(1,1,[200,150],[250,150]))
-print(polygon_left_intersection(1,1,[200,150],[250,150]))
+print(ellipse_intersection_check(1, 1, [150, 130], [150, 70]))  # False case, vertical vector cuts the ellipse
 
+print(rectangle_intersection(1, 1, [95, 20], [90, 50]))  # False case
+
+print(rhombus_intersection(1, 1, [190, 25], [260, 25]))  # False case, horizontal vector cuts the rhombus
+print(rhombus_intersection(1, 1, [225, 5], [225, 50]))  # False case, vertical vector cuts the rhombus
+
+print(polygon_right_intersection(1, 1, [10, 110], [80, 187]))
+print(polygon_left_intersection(1, 1, [10, 110], [80, 187]))
